2049034bf494e0347764015309997ceae02a2181
aminul-islam6911 <aikayoum420@gmail.com>
2022-03-19 15:12:49 +0600

    adminlogin page added

--- src/login.java
+++ src/login.java
@@ -1,4 +1,3 @@
-
 import java.awt.event.KeyEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,7 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-//hello
+
 public class login extends javax.swing.JFrame {
 
     public login() {
@@ -28,19 +27,14 @@ public class login extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         btnnewuser = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
-        txtuser1 = new javax.swing.JTextField();
         txtuser = new javax.swing.JTextField();
         btncancel = new javax.swing.JButton();
         txtpass = new javax.swing.JPasswordField();
         usrsignbt = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        txtpass1 = new javax.swing.JPasswordField();
-        adsignbt = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -59,20 +53,12 @@ public class login extends javax.swing.JFrame {
             }
         });
         jPanel2.add(btnnewuser);
-        btnnewuser.setBounds(124, 451, 87, 40);
+        btnnewuser.setBounds(300, 380, 87, 40);
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel3.setText("Password");
         jPanel2.add(jLabel3);
-        jLabel3.setBounds(69, 354, 140, 35);
-
-        txtuser1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtuser1ActionPerformed(evt);
-            }
-        });
-        jPanel2.add(txtuser1);
-        txtuser1.setBounds(660, 300, 165, 35);
+        jLabel3.setBounds(300, 290, 140, 35);
 
         txtuser.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -80,7 +66,7 @@ public class login extends javax.swing.JFrame {
             }
         });
         jPanel2.add(txtuser);
-        txtuser.setBounds(227, 301, 165, 35);
+        txtuser.setBounds(460, 240, 165, 35);
 
         btncancel.setBackground(new java.awt.Color(255, 51, 51));
         btncancel.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
@@ -91,7 +77,7 @@ public class login extends javax.swing.JFrame {
             }
         });
         jPanel2.add(btncancel);
-        btncancel.setBounds(473, 509, 100, 40);
+        btncancel.setBounds(530, 380, 100, 40);
 
         txtpass.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
@@ -99,7 +85,7 @@ public class login extends javax.swing.JFrame {
             }
         });
         jPanel2.add(txtpass);
-        txtpass.setBounds(227, 354, 165, 35);
+        txtpass.setBounds(460, 290, 165, 35);
 
         usrsignbt.setBackground(new java.awt.Color(0, 255, 0));
         usrsignbt.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
@@ -110,50 +96,32 @@ public class login extends javax.swing.JFrame {
             }
         });
         jPanel2.add(usrsignbt);
-        usrsignbt.setBounds(229, 451, 100, 40);
+        usrsignbt.setBounds(410, 380, 100, 40);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel2.setText("Username");
         jPanel2.add(jLabel2);
-        jLabel2.setBounds(69, 298, 140, 35);
-
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel4.setText("Username");
-        jPanel2.add(jLabel4);
-        jLabel4.setBounds(500, 300, 140, 35);
-
-        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel5.setText("Password");
-        jPanel2.add(jLabel5);
-        jLabel5.setBounds(500, 360, 140, 35);
-        jPanel2.add(txtpass1);
-        txtpass1.setBounds(660, 360, 165, 35);
-
-        adsignbt.setBackground(new java.awt.Color(0, 255, 0));
-        adsignbt.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
-        adsignbt.setText("Sign In");
-        adsignbt.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                adsignbtActionPerformed(evt);
-            }
-        });
-        jPanel2.add(adsignbt);
-        adsignbt.setBounds(660, 450, 100, 40);
+        jLabel2.setBounds(300, 240, 140, 35);
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel6.setText("Customer");
         jPanel2.add(jLabel6);
-        jLabel6.setBounds(123, 222, 174, 44);
-
-        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jLabel7.setText("Admin");
-        jPanel2.add(jLabel7);
-        jLabel7.setBounds(590, 230, 116, 44);
+        jLabel6.setBounds(350, 160, 174, 44);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         jLabel1.setText("Login");
         jPanel2.add(jLabel1);
         jLabel1.setBounds(360, 10, 174, 61);
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton1.setText("Are You Admin");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1);
+        jButton1.setBounds(653, 550, 190, 31);
 
         jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/rsz_tumblr_nsd09yjbo11urfkmqo1_1280.jpg"))); // NOI18N
         jPanel2.add(jLabel8);
@@ -235,47 +203,12 @@ public class login extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btncancelActionPerformed
 
-    private void txtuser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtuser1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtuser1ActionPerformed
-
-    private void adsignbtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adsignbtActionPerformed
-        if(txtuser1.getText().isEmpty() || txtpass1.getText().isEmpty()) 
-        {
-            JOptionPane.showMessageDialog(this, "Username or Password Empty");
-        } 
-        else 
-        {
-            String username = txtuser1.getText();
-            String password = txtpass1.getText();
-
-            if(username.contains("admin") && password.contains("admin")) 
-            {
-                AdminView a = new AdminView();
-                this.hide();
-                a.setVisible(true);
-            } 
-            else 
-            {
-                JOptionPane.showMessageDialog(this, "Admin Username or Password Wrong");
-                txtuser1.setText("");
-                txtpass1.setText("");
-                txtuser1.requestFocus();
-            }
-        }
-    }//GEN-LAST:event_adsignbtActionPerformed
-
     private void txtpassKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtpassKeyPressed
-        if(evt.getKeyCode() == KeyEvent.VK_ENTER) 
-        {
-            try 
-            {
-                if(txtuser.getText().isEmpty() || txtpass.getText().isEmpty())
-                {
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            try {
+                if (txtuser.getText().isEmpty() || txtpass.getText().isEmpty()) {
                     JOptionPane.showMessageDialog(this, "Username or Password Empty");
-                } 
-                else 
-                {
+                } else {
                     String username = txtuser.getText();
                     String password = txtpass.getText();
 
@@ -286,14 +219,11 @@ public class login extends javax.swing.JFrame {
                     pst.setString(2, password);
                     rs = pst.executeQuery();
 
-                    if(rs.next()) 
-                    {
+                    if (rs.next()) {
                         UserView s = new UserView();
                         this.hide();
                         s.setVisible(true);
-                    } 
-                    else
-                    {
+                    } else {
                         JOptionPane.showMessageDialog(this, "Username or Password Wrong");
                         txtuser.setText("");
                         txtpass.setText("");
@@ -301,14 +231,18 @@ public class login extends javax.swing.JFrame {
                     }
                 }
 
-            } 
-            catch (ClassNotFoundException | SQLException ex) 
-            {
+            } catch (ClassNotFoundException | SQLException ex) {
                 Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
             }
 
         }
     }//GEN-LAST:event_txtpassKeyPressed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        adminlogin al = new adminlogin();
+        this.hide();
+        al.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -346,23 +280,18 @@ public class login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton adsignbt;
     private javax.swing.JButton btncancel;
     private javax.swing.JButton btnnewuser;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPasswordField txtpass;
-    private javax.swing.JPasswordField txtpass1;
     private javax.swing.JTextField txtuser;
-    private javax.swing.JTextField txtuser1;
     private javax.swing.JButton usrsignbt;
     // End of variables declaration//GEN-END:variables
 }
